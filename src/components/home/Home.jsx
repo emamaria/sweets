ba5d6392494ec71d518605f043c56dbf55dc6d31
emamaria <emascript@gmail.com>
2022-11-02 17:18:23 +0100

    home text

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+    <div className='home__image'>
+     <img src="/assets/home/home.jpg" alt="coffe_and_cake_image"/>
+    </div>
+    <div className='home__text'>
+    <h2>Have nice time eating sweets and having a drink!</h2>
+    </div>
+    </main>
   )
 }
 
