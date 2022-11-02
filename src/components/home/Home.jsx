ed5e92d9a9482f75f0720418d3f8f8a80d561065
emamaria <emascript@gmail.com>
2022-11-02 20:33:29 +0100

    changed color

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <h2>Have a great time eating sweets and having a drink!</h2>
     </div>
     <footer> 
-    <div className='wave' style={{height: "150px",overflow: "hidden"}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100%"}}><path d="M-3.15,97.20 C114.22,23.19 273.93,-64.64 505.30,98.19 L500.00,150.00 L-8.80,167.26 Z" style={{stroke: "none", fill: "#6FEDD6"}}></path></svg></div>
+    {/* <div className='wave' style={{height: "150px",overflow: "hidden"}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100%"}}><path d="M-3.15,97.20 C114.22,23.19 273.93,-64.64 505.30,98.19 L500.00,150.00 L-8.80,167.26 Z" style={{stroke: "none", fill: "#6FEDD6"}}></path></svg></div> */}
      <div className='square'>
        <div className="address__container">
         <h2>Sweets and Drinks</h2>
