fe2861c9652142e2567d90aa4c9569390f36a0dc
emamaria <emascript@gmail.com>
2022-11-02 18:38:13 +0100

    footer wave

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main>
+    <div>
     <div className='home__image'>
      <img src="/assets/home/home.jpg" alt="coffe_and_cake_image"/>
     </div>
     <div className='home__text'>
-    <h2>Have nice time eating sweets and having a drink!</h2>
+    <h2>Have a great time eating sweets and having a drink!</h2>
     </div>
-    </main>
+    <footer> 
+    <div className='wave' style={{height: "150px",overflow: "hidden"}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100%"}}><path d="M-3.15,97.20 C114.22,23.19 273.93,-64.64 505.30,98.19 L500.00,150.00 L-8.80,167.26 Z" style={{stroke: "none", fill: "#6FEDD6"}}></path></svg></div>
+     <div className='square'></div>
+    </footer>
+    </div>
   )
 }
 
