47ee5fddd2352961309c1ed025171251d502a0f8
emamaria <emascript@gmail.com>
2022-11-03 12:13:48 +0100

    changed style

--- src/data/drinks.js
+++ src/data/drinks.js
@@ -1,3 +1,13 @@
 export const drinkData = [
-    
+    {
+        img:  `/assets/drinks/${this.name.split(" ").join("_")}.jpg`,
+        name: "Lorem",
+        price: {
+            s: 1,
+            m: 2,
+            l: 3,
+
+        } 
+      
+    }
 ]
--- src/data/sweets.js
+++ src/data/sweets.js
@@ -1,3 +1,15 @@
 export const sweetData = [
-    
+    {
+    name: "chocolate cream muffin" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "unit",    
+    },
+    {
+    name: "strawberry cream muffin" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "unit",    
+        },
+  
 ]
