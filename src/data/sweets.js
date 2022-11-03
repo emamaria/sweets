20e4d6464694a4dd88877e87596b42b5143c1e2a
emamaria <emascript@gmail.com>
2022-11-03 13:06:09 +0100

    sweets data

--- src/data/sweets.js
+++ src/data/sweets.js
@@ -11,5 +11,83 @@ export const sweetData = [
     price: 1,
     size: "unit",    
         },
+    {
+    name: "chocolate muffin" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "unit",    
+     },
+     {
+     name: "cheesecake" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "piece",    
+     },
+    {
+    name: "cream cake" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "piece",    
+    },
+    {
+    name: "brownie" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "piece",    
+        },
+     {
+     name: "tiramisu" ,
+     img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "piece",    
+                },
+    {
+    name: "carrot cake" ,
+    img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+    price: 1,
+    size: "piece",    
+     },
+     {
+        name: "matcha roll cake" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "piece",    
+    },
+    {
+        name: "butter cookie" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "unit",    
+    },
+    {
+        name: "apple pie" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "piece",    
+    },
+    {
+        name: "vanilla pudin" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "cup",    
+    },
+    {
+        name: "profiterol" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "unit",    
+    },
+    {
+        name: "chocolate_donut" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "unit",    
+    },
+    {
+        name: "glazed_donut" ,
+        img: `/assets/sweets/${this.name.split(" ").join("_")}.jpg`,
+        price: 1,
+        size: "unit",    
+    },
   
 ]
