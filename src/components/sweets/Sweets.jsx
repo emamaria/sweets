74f2e2d3a7c81b68b6d2dd15ff3a55e80353dd1b
emamaria <emascript@gmail.com>
2022-11-03 19:29:52 +0100

    ok search function input design and sweet page display

--- src/components/sweets/Sweets.jsx
+++ src/components/sweets/Sweets.jsx
@@ -7,15 +7,11 @@ const Sweets = () => {
 const [inputValue, setInputValue] = useState("");
 
 
-  let filteredData;
-
  function handleChange(event){
-  //  console.log(event.target.value)
+
    setInputValue(event.target.value)
-   filteredData = sweetData.filter( data => data.name === inputValue)
-   
-    
- }
+
+}
 
 
 
@@ -23,15 +19,16 @@ const [inputValue, setInputValue] = useState("");
 
 
   return (
-    <>
+    <div className='sweets_screen'>
     <input type="text" placeholder="write the sweet name" value={inputValue} onChange={handleChange}/>
     <div className='sweets__container'>
-      {  (inputValue.length === 0)?sweetData.map( (data, index) => {
+      {  sweetData.filter(data => data.name.toLowerCase().includes(inputValue)).map( (data, index) => {
           return <Sweet key={index}  name={data.name} price={data.price} quantity={data.size}/>
-     }): filteredData?.map((data, index) => <Sweet key={index}  name={data?.name} price={data?.price} quantity={data?.size}/> )}
-   
+        })
+      }
+      
      </div>
-    </>
+    </div>
   )
 }
 
