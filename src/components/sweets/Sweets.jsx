daaeaa9706aae2a95aa42d07c51b641247fd3227
emamaria <emascript@gmail.com>
2022-11-03 18:55:30 +0100

    input

--- src/components/sweets/Sweets.jsx
+++ src/components/sweets/Sweets.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { sweetData } from '../../data/sweets'
 import Sweet from './Sweet'
 const Sweets = () => {
 
-  
+
+const [inputValue, setInputValue] = useState("");
+
+
+  let filteredData;
+
+ function handleChange(event){
+  //  console.log(event.target.value)
+   setInputValue(event.target.value)
+   filteredData = sweetData.filter( data => data.name === inputValue)
+   
+    
+ }
+
+
+
+ 
+
+
   return (
+    <>
+    <input type="text" placeholder="write the sweet name" value={inputValue} onChange={handleChange}/>
     <div className='sweets__container'>
-      { sweetData.map( (data, index) => {
-      return <Sweet key={index}   name={data.name} price={data.price} quantity={data.size}/>
-    })}
-    </div>
+      {  (inputValue.length === 0)?sweetData.map( (data, index) => {
+          return <Sweet key={index}  name={data.name} price={data.price} quantity={data.size}/>
+     }): filteredData?.map((data, index) => <Sweet key={index}  name={data?.name} price={data?.price} quantity={data?.size}/> )}
+   
+     </div>
+    </>
   )
 }
 
