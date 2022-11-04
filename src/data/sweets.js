35c51e89d346a0b54264a241983093a9e74bf372
emamaria <emascript@gmail.com>
2022-11-04 15:23:50 +0100

    changed price

--- src/data/sweets.js
+++ src/data/sweets.js
@@ -1,47 +1,47 @@
 export const sweetData = [
     {
     name: "chocolate cream muffin" ,
-    price: 1,
+    price: 2.20,
     size: "unit",    
     },
     {
     name: "strawberry cream muffin" ,
-    price: 1,
+    price: 2.20,
     size: "unit",    
         },
     {
     name: "chocolate muffin" ,
-    price: 1,
+    price: 2.20,
     size: "unit",    
      },
      {
      name: "cheesecake" ,
-    price: 1,
+    price: 3.40,
     size: "piece",    
      },
     {
     name: "cream cake" ,
-    price: 1,
+    price: 3.20,
     size: "piece",    
     },
     {
     name: "brownie" ,
-    price: 1,
+    price: 2.80,
     size: "piece",    
         },
      {
      name: "tiramisu" ,
-     price: 1,
+     price: 3,
     size: "piece",    
                 },
     {
     name: "carrot cake" ,
-    price: 1,
+    price: 2.90,
     size: "piece",    
      },
      {
         name: "matcha roll cake" ,
-        price: 1,
+        price: 3.40,
         size: "piece",    
     },
     {
@@ -51,27 +51,27 @@ export const sweetData = [
     },
     {
         name: "apple pie with ice cream" ,
-         price: 1,
+         price: 3.20,
         size: "piece",    
     },
     {
         name: "vanilla pudin" ,
-        price: 1,
+        price: 3.60,
         size: "cup",    
     },
     {
         name: "profiterol" ,
-        price: 1,
+        price: 1.80,
         size: "unit",    
     },
     {
         name: "chocolate donut" ,
-        price: 1,
+        price: 1.20,
         size: "unit",    
     },
     {
         name: "glazed donut" ,
-        price: 1,
+        price: 1.20,
         size: "unit",    
     },
   
